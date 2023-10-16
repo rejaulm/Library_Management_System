420007d18ee715c77441889d62cc5e3a3cb49632
rejaulm <139127199+rejaulm@users.noreply.github.com>
2023-10-16 10:47:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include('header.php'); ?>
+<?php include('navbar.php'); ?>
+    <div class="container">
+		<div class="margin-top">
+			<div class="row">
+					<?php include('head.php'); ?>
+					
+					</div>The Central Library of Narula Institute of Technology is an integral part of the Institute since the establishment of the college in order to meet the information requirement of the faculty, Students & Staffs. The automated library has an enormous collection consisting of more than 84 thousands Printed books, journals, E-Journals, E-Books, Magazines, Newspaper &CD-ROMs. The goal of the Library & Documentation Centre is to support the advancement of knowledge & education excellence by offerings a rich collection development policy, automated Circulation system, book bank facility for every student, Digital zone segment, question bank repository, Display Service & E-mail service towards its clientele.
+					</br></br></br>
+				
+			
+				<div class="span2">
+				<h4></h4>
+			
+				</div>
+				<div class="span10">
+				
+				</div>
+			
+			</div>
+		</div>
+    </div>
+<?php include('footer.php') ?>
